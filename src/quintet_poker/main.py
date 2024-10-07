5bcea8ad9d35efdcb8f7d8ff3a0705e262b2be26
Daniel(Donghyun) Sung <donghyun.main@gmail.com>
2024-10-07 20:30:30 +0900

    [feature] Implement main.py for homescreen

--- src/quintet_poker/main.py
+++ src/quintet_poker/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+# Mount the static directory
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 @app.get("/")
 async def root():
-    return {"message": "Welcome to Quintet Poker!"}
+    return FileResponse("static/index.html")
